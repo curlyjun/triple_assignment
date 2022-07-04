79d748a373c2730e23e8a4ce61826271069805cf
curlyjun <tjdwns4850@gmail.com>
2022-07-04 17:01:43 +0900

    chore: favicon 변경, title 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import { globalStyle } from '@styles/globals'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>박성준 - 트리플 과제</title>
+      </Head>
       {globalStyle}
       <Component {...pageProps} />
     </>
