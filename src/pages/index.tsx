0c9f0d5cac26bc2367bd23191b27597241d8779d
curlyjun <tjdwns4850@gmail.com>
2022-07-04 16:48:43 +0900

    refactor: 페이지 스타일 코드 이동

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import type { NextPage } from 'next'
+import styled from '@emotion/styled'
 
 import { Awards } from '@components/awards'
 import { ContentLogo } from '@components/contentLogo'
 import { Metrics } from '@components/metrics'
-import { MainWrapper } from '@styles/Main.style'
+
+export const Wrapper = styled.main`
+  position: relative;
+  overflow: hidden;
+  margin: 0 auto;
+
+  width: 1200px;
+  min-width: 1200px;
+`
 
 const Main: NextPage = () => {
   return (
-    <MainWrapper>
+    <Wrapper>
       <ContentLogo />
       <Metrics />
       <Awards />
-    </MainWrapper>
+    </Wrapper>
   )
 }
 
